9cd128b148305564a958dd7d66f35cc0f3d77f28
whitesnowhahorha <yunweibai@gmail.com>
2021-11-28 17:19:48 +0800

    correct test

--- mnist/trans_distillation_train.py
+++ mnist/trans_distillation_train.py
@@ -15,6 +15,7 @@ args = parser.parse_args()
 torch.manual_seed(0)
 
 isTest = args.isTest
+CHECKTEACHER = True
 EPOCH = 1
 BATCH_SIZE = 1
 DIM = 28
@@ -59,8 +60,8 @@ class Classifier(nn.Module):
         self.classifier = nn.Linear(4, 10)
 
     def forward(self, x):
-        x = x.view(x.shape[0], -1)
         x = self.hidden1(x)
+        x = self.classifier(x)
         return x
 
 def weights_init(m):
@@ -78,7 +79,7 @@ def get_loss(out, target):
     loss = torch.square(out - target)
     return loss
 
-def train(trainloader, teacher_trans, student, teacher_cls, optimizer, scheduler, device):
+def train(trainloader, teacher_trans, student, classifier1, optimizer, scheduler, device):
     print("Training...")
     
     student.train()
@@ -92,7 +93,7 @@ def train(trainloader, teacher_trans, student, teacher_cls, optimizer, scheduler
             teacherIn = inputs.repeat(1, 3, 1, 1)
             baseline_features = teacher_trans(Variable(teacherIn).to(device).float()) # 16 * 32 * 7 * 7
             baseline_features = baseline_features.flatten(start_dim = 1)
-            baseline_features = teacher_cls(Variable(baseline_features).to(device)).flatten(start_dim = 1)
+            baseline_features = classifier1(Variable(baseline_features).to(device)).flatten(start_dim = 1)
 
             optimizer.zero_grad()
 
@@ -111,40 +112,47 @@ def train(trainloader, teacher_trans, student, teacher_cls, optimizer, scheduler
         torch.save(student.state_dict(), studentPth)
 
 
-def test(testloader, student, teacher_cls, device):
+def test(testloader, model, classifier2, device, classifier1=None):
     print("Testing...")
+    model.eval()
+    accuracy = 0
     count = 0
     for inputs, labels in testloader:
-        sample = student(Variable(inputs).to(device))
-        output = teacher_cls(Variable(sample).to(device).float())
+        if CHECKTEACHER:
+            # embedding -> 4
+            inputs = inputs.repeat(1,3,1,1)
+            sample = model(Variable(inputs).to(device)).flatten(start_dim=1)
+            sample = classifier1(Variable(sample).to(device))
+        else:
+            sample = model(Variable(inputs).to(device))
+        output = classifier2(Variable(sample).to(device))
         pred_y = torch.max(output, 1)[1].data.squeeze()
         labels = Variable(labels).to(device)
-        accuracy = (pred_y == labels).sum().item() / float(labels.size(0))
+        accuracy += (pred_y == labels).sum().item()
         count += 1
         if count % 1000 == 0:
             print(count)
-    print('Test Accuracy of the model on the 10000 test images: %.5f' % accuracy)
+    print('Test Accuracy of the model on the 10000 test images:', accuracy / 10000)
 
 def main():
     device = torch.device("cuda")
 
-    teacher_cls = Classifier()
-    teacher_cls.load_state_dict(torch.load(classifierPth))
-    for param in teacher_cls.parameters():
+    teacher_trans = ResNet50ViT(img_dim=DIM, pretrained_resnet=True, 
+                    blocks=3, classification=False, 
+                    dim_linear_block=DIM2, dim=DIM2)
+    teacher_trans.load_state_dict(torch.load(teacherPth))
+    teacher_trans.to(device)
+    for param in teacher_trans.parameters():
         param.requires_grad = False
+    teacher_cls0 = Classifier()
+    teacher_cls0.load_state_dict(torch.load(classifierPth))
+    for param in teacher_cls0.parameters():
+        param.requires_grad = False
+    classifier1 = teacher_cls0.hidden1
+    classifier1.to(device)
 
     student = CNN()
     if isTest == False:
-        teacher_cls = teacher_cls.hidden1
-        teacher_cls.to(device)
-        teacher_trans = ResNet50ViT(img_dim=DIM, pretrained_resnet=True, 
-                    blocks=3, classification=False, 
-                    dim_linear_block=DIM2, dim=DIM2)
-        teacher_trans.load_state_dict(torch.load(teacherPth))
-        teacher_trans.to(device)
-        for param in teacher_trans.parameters():
-            param.requires_grad = False
-        
         student.apply(weights_init).to(device)
         optimizer = torch.optim.Adam([
         {"params": student.hidden.parameters(), "lr": 0.001},####0.002
@@ -165,12 +173,8 @@ def main():
                                             num_workers=1)
         student.to(device)
 
-        train(trainloader, teacher_trans, student, teacher_cls, optimizer, scheduler, device)
+        train(trainloader, teacher_trans, student, classifier1, optimizer, scheduler, device)
 
-    else:
-        teacher_cls = teacher_cls.classifier
-        teacher_cls.to(device)
-        student.load_state_dict(torch.load(studentPth))
         train_data = datasets.MNIST(
             root = 'data',
             train = False,                         
@@ -182,11 +186,31 @@ def main():
                                             batch_size=BATCH_SIZE, 
                                             shuffle=True, 
                                             num_workers=1)
-        student.to(device)
-        test(testloader, student, teacher_cls, device)
+        classifier2 = teacher_cls0.classifier
+        classifier2.to(device)
+        test(testloader, student, classifier2, device)
+    else:
+        classifier2 = teacher_cls0.classifier
+        classifier2.to(device)
+        test_data = datasets.MNIST(
+            root = 'data',
+            train = False,                         
+            transform = transforms.ToTensor(),
+            download = False,            
+        )
+
+        testloader = torch.utils.data.DataLoader(test_data, 
+                                            batch_size=BATCH_SIZE, 
+                                            shuffle=True, 
+                                            num_workers=1)
+        if CHECKTEACHER == False:
+            student.load_state_dict(torch.load(studentPth))
+            student.to(device)
+            test(testloader, student, classifier2, device)
+        else:
+            test(testloader, teacher_trans, classifier2, device, classifier1)
         
     print('Done.')
 
-# 600/15, 800
 if __name__ == '__main__':
     main()
