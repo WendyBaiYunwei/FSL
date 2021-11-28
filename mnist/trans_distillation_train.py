5dd9ab9ecd056c8a21bb82135958cb8845512785
whitesnowhahorha <yunweibai@gmail.com>
2021-11-28 16:28:39 +0800

    test acc: 100%

--- mnist/trans_distillation_train.py
+++ mnist/trans_distillation_train.py
@@ -1,47 +1,66 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.autograd import Variable
-from torch import optim
 from torch.optim.lr_scheduler import StepLR
-from torchvision import datasets, models
-import torchvision
+from torchvision import datasets
 import torchvision.transforms as transforms
-from self_attention_cv import TransformerEncoder
-import numpy as np
-import cv2
+from self_attention_cv import ResNet50ViT
 import argparse
 import math
-import argparse
 
-
-## train: two lenet, mnist, one pretrained, another randomly inited, compare loss
 parser = argparse.ArgumentParser()
-parser.add_argument("-l","--learning_rate",type = float, default=0.01) # estimate: 0.2
+parser.add_argument("-test","--isTest",type = bool, default=True)
 args = parser.parse_args()
+
 torch.manual_seed(0)
 
-LEARNING_RATE = args.learning_rate
+isTest = args.isTest
 EPOCH = 1
 BATCH_SIZE = 1
-DIM = 8
-tokenSize = 4
-cropIs = [tokenSize * i for i in range(1, DIM // tokenSize + 1)]
+DIM = 28
+DIM2 = 6
+studentPth = './trans_student.pth'
+classifierPth = './base_classifier.pth'
+teacherPth = './base_trans.pth'
 
-class Encoder(nn.Module):
+class CNN(nn.Module):
     def __init__(self):
-        super(Encoder, self).__init__()
+        super(CNN, self).__init__()
         self.conv1 = nn.Sequential(         
-            nn.Linear(8 * 8, 34),                                                   
+            nn.Conv2d(
+                in_channels=1,              
+                out_channels=8,            
+                kernel_size=3,           
+                stride=1,                   
+                padding=1,                  
+            ),                              
+            nn.ReLU(),                      
+            nn.MaxPool2d(kernel_size=2),    
         )
         self.conv2 = nn.Sequential(         
-            nn.Linear(34, 8 * 8),                                        
+            nn.Conv2d(8, 8, 3, 1, 1),     
+            nn.ReLU(),                      
+            nn.MaxPool2d(2),                
         )
-        self.out = nn.Linear(8 * 8, 10)
+        self.hidden = nn.Linear(8 * 7 * 7, 4)
+        self.out = nn.Linear(4, 10)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
+        x = x.view(x.shape[0], -1)
+        x = self.hidden(x)
+        return x
+
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+        self.hidden1 = nn.Linear(DIM * DIM // 16 * DIM2, 4)
+        self.classifier = nn.Linear(4, 10)
+
+    def forward(self, x):
+        x = x.view(x.shape[0], -1)
+        x = self.hidden1(x)
         return x
 
 def weights_init(m):
@@ -56,84 +75,24 @@ def weights_init(m):
         m.bias.data = torch.ones(m.bias.data.size())
 
 def get_loss(out, target):
-    # 49, 512
     loss = torch.square(out - target)
-    # loss = torch.squeeze(loss)
     return loss
 
-def getCrops(inputs):
-    inputs = inputs.squeeze(dim = 1)#btch, channel, h, w
-    batch = np.zeros((BATCH_SIZE, (DIM ** 2) // (tokenSize ** 2), tokenSize, tokenSize))
-    for batchI, input in enumerate(inputs):
-        tokenI = 0
-        for i in cropIs:
-            for j in cropIs:
-                token = input[i - tokenSize:i, j - tokenSize:j]
-                batch[batchI, tokenI, :, :] = token
-                tokenI += 1
-    batch = torch.from_numpy(batch)
-    batch = torch.flatten(batch, start_dim = -2)
-    return batch
-    
-def main():
-    device = torch.device("cuda")
-    
-    teacher = TransformerEncoder(dim=tokenSize ** 2,blocks=3,heads=2)
-    teacher.load_state_dict(torch.load('./base_trans.pth'))
-    for param in teacher.parameters():
-        param.requires_grad = False
-    teacher.to(device)
-    student = Encoder()
-    student.apply(weights_init).to(device)
-    student.to(device)
-
-    optimizer = torch.optim.Adam(student.parameters(), lr=LEARNING_RATE)
-    scheduler = StepLR(optimizer,step_size=1,gamma=0.9)
-
-    transform = transforms.Compose(
-                [transforms.Resize((8, 8)),
-                    transforms.ToTensor(),
-                ])
-
-    # train_data_sm = datasets.MNIST(
-    #     root = 'data',
-    #     train = True,                         
-    #     transform = transform,
-    #     download = False,            
-    # )
-
-    train_data = datasets.MNIST(
-        root = 'data',
-        train = True,                         
-        transform = transform,
-        download = False,            
-    )
-
-    trainloader = torch.utils.data.DataLoader(train_data, 
-                                        batch_size=BATCH_SIZE, 
-                                        shuffle=False, 
-                                        num_workers=1)
-    # trainloader_sm = torch.utils.data.DataLoader(train_data_sm, 
-    #                                     batch_size=BATCH_SIZE, 
-    #                                     shuffle=False, 
-    #                                     num_workers=1)
-
+def train(trainloader, teacher_trans, student, teacher_cls, optimizer, scheduler, device):
     print("Training...")
     
     student.train()
 
-    def train(episode):
+    for i in range(EPOCH):
         epoch_loss = 0
         count = 0
-        # dataiter_sm = iter(trainloader_sm)
         for inputs, _ in trainloader:
-            inputs_sm = torch.flatten(inputs, start_dim = 1)
-            sample_features = student(Variable(inputs_sm).to(device))
+            sample_features = student(Variable(inputs).to(device))
 
-            inputs = getCrops(inputs)
-            baseline_features = teacher(Variable(inputs).to(device).float()).flatten(start_dim = 1) # 16 * 32 * 7 * 7
-            # inputs_sm, _ = next(dataiter_sm)
-            
+            teacherIn = inputs.repeat(1, 3, 1, 1)
+            baseline_features = teacher_trans(Variable(teacherIn).to(device).float()) # 16 * 32 * 7 * 7
+            baseline_features = baseline_features.flatten(start_dim = 1)
+            baseline_features = teacher_cls(Variable(baseline_features).to(device)).flatten(start_dim = 1)
 
             optimizer.zero_grad()
 
@@ -148,10 +107,84 @@ def main():
                 print(count, epoch_loss / (count + 1))
             count += 1
 
-    for episode in range(EPOCH):
-        train(episode)
         scheduler.step()
-        torch.save(student.state_dict(), './trans_student.pth')
+        torch.save(student.state_dict(), studentPth)
+
+
+def test(testloader, student, teacher_cls, device):
+    print("Testing...")
+    count = 0
+    for inputs, labels in testloader:
+        sample = student(Variable(inputs).to(device))
+        output = teacher_cls(Variable(sample).to(device).float())
+        pred_y = torch.max(output, 1)[1].data.squeeze()
+        labels = Variable(labels).to(device)
+        accuracy = (pred_y == labels).sum().item() / float(labels.size(0))
+        count += 1
+        if count % 1000 == 0:
+            print(count)
+    print('Test Accuracy of the model on the 10000 test images: %.5f' % accuracy)
+
+def main():
+    device = torch.device("cuda")
+
+    teacher_cls = Classifier()
+    teacher_cls.load_state_dict(torch.load(classifierPth))
+    for param in teacher_cls.parameters():
+        param.requires_grad = False
+
+    student = CNN()
+    if isTest == False:
+        teacher_cls = teacher_cls.hidden1
+        teacher_cls.to(device)
+        teacher_trans = ResNet50ViT(img_dim=DIM, pretrained_resnet=True, 
+                    blocks=3, classification=False, 
+                    dim_linear_block=DIM2, dim=DIM2)
+        teacher_trans.load_state_dict(torch.load(teacherPth))
+        teacher_trans.to(device)
+        for param in teacher_trans.parameters():
+            param.requires_grad = False
+        
+        student.apply(weights_init).to(device)
+        optimizer = torch.optim.Adam([
+        {"params": student.hidden.parameters(), "lr": 0.001},####0.002
+        {"params": student.conv1.parameters(), "lr": 0.01},
+        {"params": student.conv2.parameters(), "lr": 0.01},
+        ])
+        scheduler = StepLR(optimizer,step_size=1,gamma=0.9)
+        train_data = datasets.MNIST(
+            root = 'data',
+            train = True,                         
+            transform = transforms.ToTensor(),
+            download = False,            
+        )
+
+        trainloader = torch.utils.data.DataLoader(train_data, 
+                                            batch_size=BATCH_SIZE, 
+                                            shuffle=True, 
+                                            num_workers=1)
+        student.to(device)
+
+        train(trainloader, teacher_trans, student, teacher_cls, optimizer, scheduler, device)
+
+    else:
+        teacher_cls = teacher_cls.classifier
+        teacher_cls.to(device)
+        student.load_state_dict(torch.load(studentPth))
+        train_data = datasets.MNIST(
+            root = 'data',
+            train = False,                         
+            transform = transforms.ToTensor(),
+            download = False,            
+        )
+
+        testloader = torch.utils.data.DataLoader(train_data, 
+                                            batch_size=BATCH_SIZE, 
+                                            shuffle=True, 
+                                            num_workers=1)
+        student.to(device)
+        test(testloader, student, teacher_cls, device)
+        
     print('Done.')
 
 # 600/15, 800
