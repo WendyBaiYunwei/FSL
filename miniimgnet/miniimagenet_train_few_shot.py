413bcad4e55d7a430f73d337ef33c8437451f7f5
whitesnowhahorha <yunweibai@gmail.com>
2022-01-05 11:50:42 +0800

    Revert "get non-KD pretrained student and train with rel net 1"
    This reverts commit b822231cf0b5f28c723c90225346dca0eb973e83.

--- miniimgnet/miniimagenet_train_few_shot.py
+++ miniimgnet/miniimagenet_train_few_shot.py
@@ -11,6 +11,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 from torch.optim.lr_scheduler import StepLR
+from torchvision import datasets, models
 import numpy as np
 import task_generator_test as tg
 import os
@@ -20,7 +21,9 @@ import scipy as sp
 import scipy.stats
 
 parser = argparse.ArgumentParser(description="One Shot Visual Recognition")
-parser.add_argument("-f","--feature_dim",type = int, default = 64)
+parser.add_argument("-en","--encoder",type = int, required=True) # 1 stands for the most basic
+parser.add_argument("-o","--out_dim",type = int, default = 19) #7, 19
+parser.add_argument("-f","--feature_dim",type = int, default = 64) #64, 512
 parser.add_argument("-r","--relation_dim",type = int, default = 8)
 parser.add_argument("-w","--class_num",type = int, default = 5)
 parser.add_argument("-s","--sample_num_per_class",type = int, default = 5)
@@ -34,6 +37,8 @@ args = parser.parse_args()
 
 
 # Hyper Parameters
+ENC_TYPE = args.encoder
+OUT_DIM = args.out_dim
 FEATURE_DIM = args.feature_dim
 RELATION_DIM = args.relation_dim
 CLASS_NUM = args.class_num
@@ -52,83 +57,41 @@ def mean_confidence_interval(data, confidence=0.95):
     h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
     return m,h
 
-class CNNEncoder(nn.Module):
-    """
-    This is the standard way to define your own network in PyTorch. You typically choose the components
-    (e.g. LSTMs, linear layers etc.) of your network in the __init__ function. You then apply these layers
-    on the input step-by-step in the forward function. You can use torch.nn.functional to apply functions
-
-    such as F.relu, F.sigmoid, F.softmax, F.max_pool2d. Be careful to ensure your dimensions are correct after each
-    step. You are encouraged to have a look at the network in pytorch/nlp/model/net.py to get a better sense of how
-    you can go about defining your own network.
-
-    The documentation for all the various components available o you is here: http://pytorch.org/docs/master/nn.html
-    """
-
+class CNNEncoder1(nn.Module):
     def __init__(self):
-        """
-        We define an convolutional network that predicts the sign from an image. The components
-        required are:
+        super(CNNEncoder1, self).__init__()
+        self.conv1 = nn.Sequential(         
+            nn.Conv2d(
+                in_channels=3,              
+                out_channels=16,            
+                kernel_size=5,           
+                stride=1,                   
+                padding=2,                  
+            ),                              
+            nn.ReLU(),                      
+            nn.MaxPool2d(kernel_size=2),    
+        )
+        self.conv2 = nn.Sequential(         
+            nn.Conv2d(16, 32, 5, 1, 2),     
+            nn.ReLU(),                      
+            nn.MaxPool2d(2),      
+            nn.Conv2d(32, 64, 3, 1, 1),     
+            nn.ReLU(),
+        )
+        self.out = nn.Linear(OUT_DIM * OUT_DIM * 64, 10)
 
-        Args:
-            params: (Params) contains num_channels
-        """
-        super(CNNEncoder, self).__init__()
-        self.num_channels = 32
-        
-        # each of the convolution layers below have the arguments (input_channels, output_channels, filter_size,
-        # stride, padding). We also include batch normalisation layers that help stabilise training.
-        # For more details on how to use these layers, check out the documentation.
-        self.conv1 = nn.Conv2d(3, self.num_channels, 3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(self.num_channels)
-        self.conv2 = nn.Conv2d(self.num_channels, self.num_channels*2, 3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(self.num_channels*2)
-        self.conv3 = nn.Conv2d(self.num_channels*2, self.num_channels*4, 3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(self.num_channels*4)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        # x = x.flatten(start_dim = 1)
+        # y = self.out(x)
+        return x
 
-        # 2 fully connected layers to transform the output of the convolution layers to the final output
-        self.fc1 = nn.Linear(4*4*self.num_channels*4, self.num_channels*4)
-        self.fcbn1 = nn.BatchNorm1d(self.num_channels*4)
-        self.fc2 = nn.Linear(self.num_channels*4, 10)       
-        self.dropout_rate = 0.5
-
-    def forward(self, s):
-        """
-        This function defines how we use the components of our network to operate on an input batch.
-
-        Args:
-            s: (Variable) contains a batch of images, of dimension batch_size x 3 x 32 x 32 .
-
-        Returns:
-            out: (Variable) dimension batch_size x 6 with the log probabilities for the labels of each image.
-
-        Note: the dimensions after each step are provided
-        """
-        #                                                  -> batch_size x 3 x 32 x 32
-        # we apply the convolution layers, followed by batch normalisation, maxpool and relu x 3
-        s = self.bn1(self.conv1(s))                         # batch_size x num_channels x 32 x 32
-        s = F.relu(F.max_pool2d(s, 2))                      # batch_size x num_channels x 16 x 16
-        s = self.bn2(self.conv2(s))                         # batch_size x num_channels*2 x 16 x 16
-        s = F.relu(F.max_pool2d(s, 2))                      # batch_size x num_channels*2 x 8 x 8
-        s = self.bn3(self.conv3(s))                         # batch_size x num_channels*4 x 8 x 8
-        s = F.relu(F.max_pool2d(s, 2))                      # batch_size x num_channels*4 x 4 x 4
-
-        # # flatten the output for each image
-        # s = s.view(-1, 4*4*self.num_channels*4)             # batch_size x 4*4*num_channels*4
-
-        # # apply 2 fully connected layers with dropout
-        # s = F.dropout(F.relu(self.fcbn1(self.fc1(s))), 
-        #     p=self.dropout_rate, training=self.training)    # batch_size x self.num_channels*4
-        # s = self.fc2(s)                                     # batch_size x 10
-
-        return s
-
-class RelationNetwork(nn.Module):
-    """docstring for RelationNetwork"""
-    def __init__(self,input_size,hidden_size):
-        super(RelationNetwork, self).__init__()
+class CNNEncoder2(nn.Module):
+    def __init__(self):
+        super(CNNEncoder2, self).__init__()
         self.layer1 = nn.Sequential(
-                        nn.Conv2d(64*2,64,kernel_size=3,padding=0),
+                        nn.Conv2d(3,64,kernel_size=3,padding=0),
                         nn.BatchNorm2d(64, momentum=1, affine=True),
                         nn.ReLU(),
                         nn.MaxPool2d(2))
@@ -137,7 +100,64 @@ class RelationNetwork(nn.Module):
                         nn.BatchNorm2d(64, momentum=1, affine=True),
                         nn.ReLU(),
                         nn.MaxPool2d(2))
+        self.layer3 = nn.Sequential(
+                        nn.Conv2d(64,64,kernel_size=3,padding=1),
+                        nn.BatchNorm2d(64, momentum=1, affine=True),
+                        nn.ReLU())
+        self.layer4 = nn.Sequential(
+                        nn.Conv2d(64,64,kernel_size=3,padding=1),
+                        nn.BatchNorm2d(64, momentum=1, affine=True),
+                        nn.ReLU())
+        self.final = nn.Linear(OUT_DIM * OUT_DIM * 64, 10)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        # out = out.flatten(start_dim = 1)
+        # y = self.final(out)
+        return out
+
+class RelationNetwork(nn.Module):
+    """docstring for RelationNetwork"""
+    def __init__(self,input_size,hidden_size):
+        super(RelationNetwork, self).__init__()
+        self.layer1 = nn.Sequential(
+                        nn.Conv2d(FEATURE_DIM*2,FEATURE_DIM,kernel_size=3,padding=0),
+                        nn.BatchNorm2d(FEATURE_DIM, momentum=1, affine=True),
+                        nn.ReLU(),
+                        nn.MaxPool2d(2))
+        self.layer2 = nn.Sequential(
+                        nn.Conv2d(FEATURE_DIM,FEATURE_DIM,kernel_size=3,padding=0),
+                        nn.BatchNorm2d(FEATURE_DIM, momentum=1, affine=True),
+                        nn.ReLU(),
+                        nn.MaxPool2d(2))
         self.fc1 = nn.Linear(input_size*3*3,hidden_size)
+        self.fc2 = nn.Linear(hidden_size,1)
+
+    def forward(self,x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0),-1)
+        out = F.relu(self.fc1(out))
+        out = F.sigmoid(self.fc2(out))
+        return out
+
+class RelationNetwork2(nn.Module):
+    """docstring for RelationNetwork"""
+    def __init__(self,input_size,hidden_size):
+        super(RelationNetwork2, self).__init__()
+        self.layer1 = nn.Sequential(
+                        nn.Conv2d(FEATURE_DIM*2,FEATURE_DIM,kernel_size=3,padding=1),
+                        nn.BatchNorm2d(FEATURE_DIM, momentum=1, affine=True),
+                        nn.ReLU(),
+                        nn.MaxPool2d(2))
+        self.layer2 = nn.Sequential(
+                        nn.Conv2d(FEATURE_DIM,FEATURE_DIM//2,kernel_size=3,padding=1),
+                        nn.BatchNorm2d(FEATURE_DIM//2, momentum=1, affine=True),
+                        nn.ReLU())
+        self.fc1 = nn.Linear(256*3*3,hidden_size)
         self.fc2 = nn.Linear(hidden_size,1)
 
     def forward(self,x):
@@ -172,10 +192,23 @@ def main():
     # Step 2: init neural networks
     print("init neural networks")
 
-    feature_encoder = CNNEncoder()
-    relation_network = RelationNetwork(FEATURE_DIM,RELATION_DIM)
+    if ENC_TYPE == 1:
+        feature_encoder = CNNEncoder1()
+    elif ENC_TYPE == 2:
+        feature_encoder = CNNEncoder2()
+    else:
+        teacher = models.vgg16(pretrained=False)
+        teacher.load_state_dict(torch.load('./vgg16.pth'))
+        feature_encoder = teacher.features
+        for param in feature_encoder.parameters():
+            param.requires_grad = False
+    
+    if ENC_TYPE == 1 or ENC_TYPE == 2:
+        relation_network = RelationNetwork(FEATURE_DIM,RELATION_DIM)
+    else:
+        relation_network = RelationNetwork2(FEATURE_DIM // 2,RELATION_DIM)
 
-    feature_encoder.load_state_dict(torch.load(os.path.abspath('/home/vipuser/Documents/knowledge-distillation-pytorch/experiments/cnn_distill/best.pth.tar'))['state_dict'])
+    feature_encoder.apply(weights_init)
     relation_network.apply(weights_init)
 
     feature_encoder.cuda(GPU)
@@ -186,11 +219,11 @@ def main():
     relation_network_optim = torch.optim.Adam(relation_network.parameters(),lr=LEARNING_RATE)
     relation_network_scheduler = StepLR(relation_network_optim,step_size=100000,gamma=0.5)
 
-    # if os.path.exists(str("./models/miniimagenet_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")):
-    #     feature_encoder.load_state_dict(torch.load(str("./models/miniimagenet_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")))
+    # if os.path.exists(str("./models/miniimagenet_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot" + str(ENC_TYPE) + ".pkl")):
+    #     feature_encoder.load_state_dict(torch.load(str("./models/miniimagenet_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot" + str(ENC_TYPE) + ".pkl")))
     #     print("load feature encoder success")
-    # if os.path.exists(str("./models/miniimagenet_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")):
-    #     relation_network.load_state_dict(torch.load(str("./models/miniimagenet_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")))
+    # if os.path.exists(str("./models/miniimagenet_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot" + str(ENC_TYPE) + ".pkl")):
+    #     relation_network.load_state_dict(torch.load(str("./models/miniimagenet_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot" + str(ENC_TYPE) + ".pkl")))
     #     print("load relation network success")
 
     # Step 3: build graph
@@ -207,16 +240,19 @@ def main():
         # sample_dataloader is to obtain previous samples for compare
         # batch_dataloader is to batch samples for training
         task = tg.MiniImagenetTask(metatrain_folders,CLASS_NUM,SAMPLE_NUM_PER_CLASS,BATCH_NUM_PER_CLASS)
-        sample_dataloader = tg.get_mini_imagenet_data_loader(task,num_per_class=SAMPLE_NUM_PER_CLASS,split="train",shuffle=False)
-        batch_dataloader = tg.get_mini_imagenet_data_loader(task,num_per_class=BATCH_NUM_PER_CLASS,split="test",shuffle=True)
+        sample_dataloader = tg.get_mini_imagenet_data_loader(task,ENC_TYPE,num_per_class=SAMPLE_NUM_PER_CLASS,split="train",shuffle=False)
+        batch_dataloader = tg.get_mini_imagenet_data_loader(task,ENC_TYPE,num_per_class=BATCH_NUM_PER_CLASS,split="test",shuffle=True)
 
         # sample datas
         samples,sample_labels = sample_dataloader.__iter__().next() #25*3*84*84
         batches,batch_labels = batch_dataloader.__iter__().next()
 
         # calculate features
-        sample_features = feature_encoder(Variable(samples).cuda(GPU)) # 25*64*19*19
-        sample_features = sample_features.view(CLASS_NUM,SAMPLE_NUM_PER_CLASS,FEATURE_DIM,19,19)
+        sample_features = feature_encoder(Variable(samples).cuda(GPU)) # 25*64*OUT_DIM*OUT_DIM
+        if ENC_TYPE == 1 or ENC_TYPE == 2:
+            sample_features = sample_features.view(CLASS_NUM,SAMPLE_NUM_PER_CLASS,FEATURE_DIM,OUT_DIM,OUT_DIM)
+        else:
+            sample_features = sample_features.view(CLASS_NUM,SAMPLE_NUM_PER_CLASS,FEATURE_DIM,7,7)
         sample_features = torch.sum(sample_features,1).squeeze(1)
         batch_features = feature_encoder(Variable(batches).cuda(GPU)) # 20x64*5*5
 
@@ -226,7 +262,10 @@ def main():
         sample_features_ext = sample_features.unsqueeze(0).repeat(BATCH_NUM_PER_CLASS*CLASS_NUM,1,1,1,1)
         batch_features_ext = batch_features.unsqueeze(0).repeat(CLASS_NUM,1,1,1,1)
         batch_features_ext = torch.transpose(batch_features_ext,0,1)
-        relation_pairs = torch.cat((sample_features_ext,batch_features_ext),2).view(-1,FEATURE_DIM*2,19,19)
+        if ENC_TYPE == 1 or ENC_TYPE == 2:
+            relation_pairs = torch.cat((sample_features_ext,batch_features_ext),2).view(-1,FEATURE_DIM*2,OUT_DIM,OUT_DIM)
+        else:
+            relation_pairs = torch.cat((sample_features_ext,batch_features_ext),2).view(-1,FEATURE_DIM*2,7,7)
         relations = relation_network(relation_pairs).view(-1,CLASS_NUM)
 
         mse = nn.MSELoss().cuda(GPU)
@@ -249,7 +288,7 @@ def main():
 
 
         if (episode+1)%100 == 0:
-                print("episode:",episode+1,"loss",loss.data[0])
+                print("episode:",episode+1,"loss",loss.item())
 
         if episode%5000 == 0:
 
@@ -259,16 +298,19 @@ def main():
             for i in range(TEST_EPISODE):
                 total_rewards = 0
                 task = tg.MiniImagenetTask(metatest_folders,CLASS_NUM,SAMPLE_NUM_PER_CLASS,15)
-                sample_dataloader = tg.get_mini_imagenet_data_loader(task,num_per_class=SAMPLE_NUM_PER_CLASS,split="train",shuffle=False)
+                sample_dataloader = tg.get_mini_imagenet_data_loader(task,ENC_TYPE,num_per_class=SAMPLE_NUM_PER_CLASS,split="train",shuffle=False)
                 num_per_class = 5
-                test_dataloader = tg.get_mini_imagenet_data_loader(task,num_per_class=num_per_class,split="test",shuffle=False)
+                test_dataloader = tg.get_mini_imagenet_data_loader(task,ENC_TYPE,num_per_class=num_per_class,split="test",shuffle=False)
 
                 sample_images,sample_labels = sample_dataloader.__iter__().next()
                 for test_images,test_labels in test_dataloader:
                     batch_size = test_labels.shape[0]
                     # calculate features
                     sample_features = feature_encoder(Variable(sample_images).cuda(GPU)) # 5x64
-                    sample_features = sample_features.view(CLASS_NUM,SAMPLE_NUM_PER_CLASS,FEATURE_DIM,19,19)
+                    if ENC_TYPE == 1 or ENC_TYPE == 2:
+                        sample_features = sample_features.view(CLASS_NUM,SAMPLE_NUM_PER_CLASS,FEATURE_DIM,OUT_DIM,OUT_DIM)
+                    else:
+                        sample_features = sample_features.view(CLASS_NUM,SAMPLE_NUM_PER_CLASS,FEATURE_DIM,7,7)
                     sample_features = torch.sum(sample_features,1).squeeze(1)
                     test_features = feature_encoder(Variable(test_images).cuda(GPU)) # 20x64
 
@@ -279,7 +321,10 @@ def main():
 
                     test_features_ext = test_features.unsqueeze(0).repeat(1*CLASS_NUM,1,1,1,1)
                     test_features_ext = torch.transpose(test_features_ext,0,1)
-                    relation_pairs = torch.cat((sample_features_ext,test_features_ext),2).view(-1,FEATURE_DIM*2,19,19)
+                    if ENC_TYPE == 1 or ENC_TYPE == 2:
+                        relation_pairs = torch.cat((sample_features_ext,test_features_ext),2).view(-1,FEATURE_DIM*2,OUT_DIM,OUT_DIM)
+                    else:
+                        relation_pairs = torch.cat((sample_features_ext,test_features_ext),2).view(-1,FEATURE_DIM*2,OUT_DIM,OUT_DIM)
                     relations = relation_network(relation_pairs).view(-1,CLASS_NUM)
 
                     _,predict_labels = torch.max(relations.data,1)
@@ -300,16 +345,12 @@ def main():
             if test_accuracy > last_accuracy:
 
                 # save networks
-                torch.save(feature_encoder.state_dict(),str("./models/miniimagenet_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl"))
-                torch.save(relation_network.state_dict(),str("./models/miniimagenet_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl"))
+                torch.save(feature_encoder.state_dict(),str("./models/miniimagenet_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot" + str(ENC_TYPE) + ".pkl"))
+                torch.save(relation_network.state_dict(),str("./models/miniimagenet_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot" + str(ENC_TYPE) + ".pkl"))
 
                 print("save networks for episode:",episode)
 
                 last_accuracy = test_accuracy
-
-
-
-
 
 if __name__ == '__main__':
     main()
